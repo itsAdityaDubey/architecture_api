736e3251a93b68ebda108815acfe0b04edf71aa3
Aditya Dubey <emailadityadubey@gmail.com>
2022-07-18 04:58:28 +0530

    Updated DB for Online MONGO DB

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const dbName = "stplayer";
+
 //  For Server
-// const dbName = "stplayer";
-// `mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@${dbName}.v2fuw.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(`mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@${dbName}.v2fuw.mongodb.net/?retryWrites=true&w=majority`,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
 
 // For Local use
-mongoose.connect(`mongodb://localhost:27017/stplayer`,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+// mongoose.connect(`mongodb://localhost:27017/${dbName}`,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
 
 console.log("Connection Successfull");
 }).catch((e)=>{
